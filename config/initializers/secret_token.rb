a6f39fa996c70b6c60ccd333b4fb98b47d4184b4
Emilio Losada <emilio.losada@udc.es>
2013-05-13 13:16:52 -0400

    All

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GdI18n::Application.config.secret_token = '08ba5d37ef937df377f56ba91a5b1dd39012785abe7f52d0973e68402db9a0704cb1671b0b8fb6b5aa3da60ddc0a411b25fb3e757e46e3dff8bd12d4b4d0cb64'
